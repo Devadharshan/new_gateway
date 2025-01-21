6a5c3d2d40f3aadd0f40477132afe9427f79a6ae
Devadharshan <59635817+Devadharshan@users.noreply.github.com>
2025-01-22 00:22:47 +0530

    Update t.sql

--- t.sql
+++ t.sql
@@ -1,24 +1,21 @@
 CREATE VIEW v_task_with_request AS
 SELECT
-    vt.number_ AS task_number,                 -- Task number
-    vt.short_description,                     -- Short description of the task
-    vt.catalog_type AS catalog_item,          -- Catalog item of the ticket
-    vt.closed_notes AS close_notes,           -- Close notes field
-    vt.assigned_to,                           -- Assigned to user
-    vt.assignment_group,                      -- Assignment group responsible
-    vt.opened_at,                             -- When the ticket was opened
-    vt.closed_at,                             -- When the ticket was closed
-    vt.opened_by,                             -- User who opened the ticket
-    vt.priority,                              -- Priority of the task
-    vt.state_ AS state,                       -- Current state of the task
-    vt.sys_created_by,                        -- User who created the task
-    vt.sys_created_on,                        -- Timestamp when the task was created
-    vt.time_worked,                           -- Total time worked on the task
-    vt.u_tags,                                -- Custom tags associated with the task
-    sr.number AS request_number,              -- Associated request number
-    -- Calculated Fields
-    TIMESTAMPDIFF(SECOND, vt.opened_at, vt.closed_at) AS time_to_close_seconds, -- Time to close in seconds
-    TIMESTAMPDIFF(HOUR, vt.opened_at, vt.closed_at) / 24 AS time_to_close_days  -- Time to close in days
+    vt.number_ AS task_number,         -- Task number
+    vt.short_description,             -- Short description of the task
+    vt.catalog_type AS catalog_item,  -- Catalog item of the ticket
+    vt.closed_notes AS close_notes,   -- Close notes field
+    vt.assigned_to,                   -- Assigned to user
+    vt.assignment_group,              -- Assignment group responsible
+    vt.opened_at,                     -- When the ticket was opened
+    vt.closed_at,                     -- When the ticket was closed
+    vt.opened_by,                     -- User who opened the ticket
+    vt.priority,                      -- Priority of the task
+    vt.state_ AS state,               -- Current state of the task
+    vt.sys_created_by,                -- User who created the task
+    vt.sys_created_on,                -- Timestamp when the task was created
+    vt.time_worked,                   -- Total time worked on the task
+    vt.u_tags,                        -- Custom tags associated with the task
+    sr.number AS request_number       -- Associated request number
 FROM
     v_task vt
 LEFT JOIN
